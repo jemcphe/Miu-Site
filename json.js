2cabd15f1aa347e8ae67fa3f4937838264fc3ef7
James McPherson <jemcphe@fullsail.edu>
2012-03-25 23:02:54 -0500

    Adding Additional Objects to json.js
    20 Total Players

--- json.js
+++ json.js
@@ -59,5 +59,131 @@ var json = {
 				"starter"	: ["Starter: ", "Yes"],
 				"skill"			: ["Skill: ", "9"],
 				"notes"		: ["Notes: ", "Great Pass Rush, Shut Down Defense"]
+		},
+		"player7": {
+				"position": ["Position: ", "QB"],
+				"pname"	: ["Player Name: ", "Aaron Rodgers"],
+				"team"		: ["Team Name: ", "Green Bay Packers"],
+				"bye"			: ["Bye Week: ", "12/01/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Possible MVP for 2012"]
+		},
+		"player8": {
+				"position": ["Position: ", "QB"],
+				"pname"	: ["Player Name: ", "Drew Brees"],
+				"team"		: ["Team Name: ", "New Orleans Sains"],
+				"bye"			: ["Bye Week: ", "11/11/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "8"],
+				"notes"		: ["Notes: ", "Natural Born Leader"]
+		},
+		"player9": {
+				"position": ["Position: ", "RB"],
+				"pname"	: ["Player Name: ", "Arian Foster"],
+				"team"		: ["Team Name: ", "Houston Texans"],
+				"bye"			: ["Bye Week: ", "10/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "10"],
+				"notes"		: ["Notes: ", "Must Have RB... My Absolute #1 pick"]
+		},
+		"player10": {
+				"position": ["Position: ", "RB"],
+				"pname"	: ["Player Name: ", "Ray Rice"],
+				"team"		: ["Team Name: ", "Baltimore Ravens"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Great 2nd RB."]
+		},
+		"player11": {
+				"position": ["Position: ", "WR"],
+				"pname"	: ["Player Name: ", "Calvin Johnson"],
+				"team"		: ["Team Name: ", "Detroit Lions"],
+				"bye"			: ["Bye Week: ", "12/18/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Big Guy, Fast, Catches Everything... Scary"]
+		},
+		"player12": {
+				"position": ["Position: ", "WR"],
+				"pname"	: ["Player Name: ", "Wes Welker"],
+				"team"		: ["Team Name: ", "New England Patriots"],
+				"bye"			: ["Bye Week: ", "11/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Little man catches absolutely everything."]
+		},
+		"player13": {
+				"position": ["Position: ", "WR"],
+				"pname"	: ["Player Name: ", "Victor Cruz"],
+				"team"		: ["Team Name: ", "New York Giants"],
+				"bye"			: ["Bye Week: ", "10/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "8"],
+				"notes"		: ["Notes: ", "Pulls off some amazing catches.. high points producer."]
+		},
+		"player14": {
+				"position": ["Position: ", "TE"],
+				"pname"	: ["Player Name: ", "Jimmy Graham"],
+				"team"		: ["Team Name: ", "New Orleans Saints"],
+				"bye"			: ["Bye Week: ", "11/11/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Big, Quick, familiar with the endzone."]
+		},
+		"player15": {
+				"position": ["Position: ", "TE"],
+				"pname"	: ["Player Name: ", "Jason Witten"],
+				"team"		: ["Team Name: ", "Dallas Cowboys"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "No"],
+				"skill"			: ["Skill: ", "7"],
+				"notes"		: ["Notes: ", "Great TE/WR for Bye Week fill in."]
+		},
+		"player16": {
+				"position": ["Position: ", "TE"],
+				"pname"	: ["Player Name: ", "Vernon Davis"],
+				"team"		: ["Team Name: ", "SanFrancisco 49ers"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "No"],
+				"skill"			: ["Skill: ", "6"],
+				"notes"		: ["Notes: ", "Hit and Miss on points, but big time points when he's on."]
+		},
+		"player17": {
+				"position": ["Position: ", "K"],
+				"pname"	: ["Player Name: ", "David Akers"],
+				"team"		: ["Team Name: ", "SanFrancisco 49ers"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Never Misses"]
+		},
+		"player18": {
+				"position": ["Position: ", "K"],
+				"pname"	: ["Player Name: ", "Dan Bailey"],
+				"team"		: ["Team Name: ", "Dallas Cowboys"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "No"],
+				"skill"			: ["Skill: ", "7"],
+				"notes"		: ["Notes: ", "Probably able to pick from free agency."]
+		},
+		"player19": {
+				"position": ["Position: ", "DEF"],
+				"pname"	: ["Player Name: ", "Chicago Bears"],
+				"team"		: ["Team Name: ", "Chicago Bears"],
+				"bye"			: ["Bye Week: ", "11/19/2012"],
+				"starter"	: ["Starter: ", "No"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Great Pass Rush, Forces Turnovers"]
+		},
+		"player20": {
+				"position": ["Position: ", "DEF"],
+				"pname"	: ["Player Name: ", "Baltimore Ravens"],
+				"team"		: ["Team Name: ", "Baltimore Ravens"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Ray Lewis makes this defense work... HARD!"]
 		}
 }
